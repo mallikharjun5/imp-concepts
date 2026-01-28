fe6d978d2b48acadfa9b11e7f5752a577332b9de
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-28 21:15:27 +0530

    disk usage warning

---
+++ disk_usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+threshold_critical=90
+threshold_warning=80
+
+current_usage=$(df -h / | awk 'NR==2 {print$5}'| cut -d '%' -f1)
+
+echo "current usage is: $current_usage"
+
+if [ $current_usage -ge $threshold_critical ; then
+    echo "Disk usage is critical ($current_usage%)" please take immediate action
+
+elif [ $current_usage -ge $threshold_warning ]; then
+    echo "Disk usage is high ($current_usage%)" please cleanup the disk space
+
+else
+    echo "Disk usage is normal ($current_usage)"
+fi
