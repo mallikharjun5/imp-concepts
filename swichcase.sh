6f9460ba4dd9156a7bf1a8d72e688a0847cb80cf
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-28 21:50:19 +0530

    switch case

---
+++ swichcase.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "1. display date"
+echo "2. display calender"
+echo "3. display current working directory"
+echo "4. Exit"
+
+read -p "Enter your choice:" choice
+
+case $choice in
+
+1)
+    date
+    ;;
+2)
+    cal
+    ;;
+3)
+    pwd
+    ;;
+4)
+    echo ""Exiting program"
+    exit 0
+    ;;
+*)
+    echo "Invalid choice: $choice please enter number between 1 and 4
+    ;;
+esac
