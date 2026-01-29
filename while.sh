4af1e64643985fe839fe1d25acd8d484f46217b3
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-29 11:49:39 +0530

    while

---
+++ while.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter names (type 'exit' to quit):"
+while :
+do
+    read -p "Name:" name
+    if [ "$Name" = "exit"]; then
+    break
+    fi
+    echo "Hello ---> $name!"
+done
+    echo "you are exited"
