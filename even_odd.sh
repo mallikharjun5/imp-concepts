b372e68f488e4574838d7b92b2bb7aee2acdb209
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-30 20:54:30 +0530

    even odd

---
+++ even_odd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# FUNCTION TO CHECK IF A NUMBER US EVEN OR ODD
+
+check_even_odd() {
+    local number="$1"
+    if (( number % 2 == 0 )); then
+        echo "number is even"
+    else
+        echo "number is odd"
+    fi
+}
+
+for (( i=1; i<=5; i++ ))
+{
+    read -p "enter a number:" num
+    if [ $Num -gt -1]; then
+    check_even_odd num
+    else
+        echo "please enter valid input"
+    fi
+}
